5a72b099555a5f098bc39d572361c9d8f16c869f
tdedetd <tdedetd@gmail.com>
2021-08-12 14:24:48 +0300

    prevent sending request if query is empty

--- src/app/store/repo.effects.ts
+++ src/app/store/repo.effects.ts
@@ -1,9 +1,11 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { Store } from "@ngrx/store";
+import { of } from "rxjs";
 import { map, switchMap, withLatestFrom } from "rxjs/operators";
 
 import { AppState } from ".";
+import { SearchRepoResponse } from "../models/search-repo-response";
 import { GithubService } from "../services/github.service";
 import { ERepoActions, LoadRepositories, LoadRepositoriesSuccess } from "./repo.actions";
 
@@ -14,13 +16,23 @@ export class RepoEffects {
     ofType(ERepoActions.LoadRepositories),
     withLatestFrom(this.store$),
     map(([_, state]) => state.repo),
-    switchMap(repoState => this.github.getRepositories({
-      q: repoState.filters.query,
-      sort: repoState.filters.sort,
-      order: repoState.filters.order,
-      per_page: repoState.perPage,
-      page: repoState.nextPage
-    })),
+    switchMap(repoState => {
+      if (!repoState.filters.query) {
+        return of({
+          incomplete_results: false,
+          items: [],
+          total_count: 0
+        } as SearchRepoResponse);
+      }
+
+      return this.github.getRepositories({
+        q: repoState.filters.query,
+        sort: repoState.filters.sort,
+        order: repoState.filters.order,
+        per_page: repoState.perPage,
+        page: repoState.nextPage
+      })
+    }),
     map(data => new LoadRepositoriesSuccess({ repos: data.items, total: data.total_count }))
   ));
 
